6f9d385726a48520ecb2216cc919bba306beabb9
mahmoud fathy <mahmoud.fathy.abbas.123@gmail.com>
2025-02-03 22:32:08 +0200

    Update welcome.java
    edditor

--- welcome.java
+++ welcome.java
@@ -3,6 +3,9 @@ public class welcome {
         System.out.println("hello");
         System.out.println("mahmoud");
         System.out.println("hello malika");
+        int x =10; int y = 20;
+        int sum = x=y;
+        System.out.println(sum);
 
         
     }
