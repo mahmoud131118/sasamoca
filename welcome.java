b8b82200bc63715539c3255d34d0344276f30961
mahmoud131118 <mahmoud.fathy.abbas.123@gmail.com>
2025-02-03 21:59:15 +0200

    for fun

--- welcome.java
+++ welcome.java
@@ -1,6 +1,8 @@
 public class welcome {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("mahmoud");
+        System.out.println("hello malika");
 
         
     }
